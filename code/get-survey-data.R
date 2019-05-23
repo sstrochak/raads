c215cc7c31c11e67461a1bd9bf74312ce3387e28
Strochak <sstrochak@urban.org>
2019-05-23 09:28:41 -0400

    remove faulty gs command

--- code/get-survey-data.R
+++ code/get-survey-data.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(googlesheets)
 
-gs_ls()
+# gs_ls()
 
 gradschool <- gs_url("https://docs.google.com/spreadsheets/d/13f48MICmqYNxkrEajNeE2Ai-TKuoJBVDzvGFcFeP37s/edit#gid=744445016")
 
